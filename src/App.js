e6a90205cef30680fa4173f2b6d21180f708f952
tsudodog <malamute29@gmail.com>
2018-10-04 23:57:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 import './App.css';
+import HeaderTabs from './HeaderTabs';
 
+
+
+
+const Timeline = (props) => {
+
+  return(
+    <div>
+    
+    </div>
+  );
+}
+
+
+
+const Footer = (props) =>{
+
+  return(
+    <h1>{props.title}</h1>
+  )
+
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HeaderTabs></HeaderTabs>
+        <Button variant="contained" color="primary">Hello World!</Button>
+        <Footer title="this goes in the footer"></Footer>
       </div>
     );
   }
